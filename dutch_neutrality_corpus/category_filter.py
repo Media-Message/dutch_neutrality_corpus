cc11c93de8d5da65ac1f5b96d3d37bcbf5e81507
Nick Martin <nickleomartin@gmail.com>
2021-02-11 10:05:05 +0000

    Remove legacy code comments

--- dutch_neutrality_corpus/category_filter.py
+++ dutch_neutrality_corpus/category_filter.py
@@ -1,21 +1,16 @@
-import re
 from bs4 import BeautifulSoup
 
 CATEGORIES_TO_FILTER = ['Categorieën', 'Categorie']
 CATEGORY_REGEX = 'Categorie:*'
 
 
+# TODO: "Twijfel aan de neutraliteit"
 def extract_categories(html):
-    # category_html = None
     soup = BeautifulSoup(html, features='html.parser')
-
     categories_html = soup.select("a[title*=Categorie]")
-
     categories = [a.text for a in categories_html
                   if a.text not in CATEGORIES_TO_FILTER]
     print(categories)
-
-    # TODO: "Twijfel aan de neutraliteit"
     return categories
 
 
@@ -23,11 +18,4 @@ def apply_category_filter(row):
     html = row['html_content']
     revision_categories = extract_categories(html=html)
     row['categories'] = revision_categories
-    # print('categories: ', revision_categories)
     return row
-
-    # # TODO: correct later...
-    # if CATEGORIES_TO_FILTER in revision_categories:
-
-    #     return row
-    # return {}
